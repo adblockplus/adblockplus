0b919e427c091360e64072be69b22ea019def066
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-10-04 15:28:33 +0200

    NoIssue - amended comment about window.close

--- js/popup.utils.js
+++ js/popup.utils.js
@@ -37,6 +37,7 @@ function reportIssue(tab)
     url: browser.runtime.getURL("/issue-reporter.html?" + tab.id)
   }).then(
     // force closing popup which is not happening in Firefox
+    // @link https://issues.adblockplus.org/ticket/7017
     () => window.close()
   );
 }
