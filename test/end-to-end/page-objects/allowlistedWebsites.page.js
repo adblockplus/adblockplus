060d79d6e9639d727140500ad811c400337ab265
ArCiGo <armandocifuentes_1@hotmail.com>
2023-02-22 21:50:36 -0600

    Update of tab

--- test/end-to-end/page-objects/allowlistedWebsites.page.js
+++ test/end-to-end/page-objects/allowlistedWebsites.page.js
@@ -30,7 +30,7 @@ class AllowlistedWebsitesPage extends BasePage
   get _allowlistedWebsitesTabButton()
   {
     return $("//a[contains(@data-i18n, 'options_tab_allowlist')" +
-        "and text()='Allowlisted websites']");
+        "and text()='QA task: Armando Cifuentes']");
   }
 
   async init()
