4c65a58cad6dde1e7d6bbaa682262ded00f2305e
Dorel Gabor <d.gabor@eyeo.com>
2024-05-31 17:37:42 +0000

    test: Added ABP DOM injection test cases [#1659]

---
+++ test/end-to-end/test-data/data-abp-dom-injection.js
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+const abpDomInjectionData = [
+  {
+    testName: "Adblock Plus",
+    url: "https://adblockplus.org/"
+  },
+  {
+    testName: "Accounts ABP",
+    url: "https://accounts.adblockplus.org/"
+  },
+  {
+    testName: "New ABP",
+    url: "https://new.adblockplus.org/"
+  },
+  {
+    testName: "Get AdBlock",
+    url: "https://getadblock.com/"
+  },
+  {
+    testName: "VPN",
+    url: "https://vpn.getadblock.com/"
+  }
+];
+
+exports.abpDomInjectionData = abpDomInjectionData;
---
+++ test/end-to-end/tests/test-abp-dom-injection-premium-user.js
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+const {beforeSequence, switchToABPOptionsTab,
+       executeAsyncScript, enablePremiumByMockServer} = require("../helpers");
+const {expect} = require("chai");
+const abpDomInjectionData =
+  require("../test-data/data-abp-dom-injection").abpDomInjectionData;
+let appVersion;
+let id;
+
+describe("test abp DOM injection for premium user", function()
+{
+  before(async function()
+  {
+    await beforeSequence();
+    appVersion = await browser.
+      executeScript("return browser.runtime.getManifest().version;", []);
+    id = await executeAsyncScript("return browser.runtime." +
+      "sendMessage({type: 'prefs.get', key: 'installation_id'});");
+    await enablePremiumByMockServer();
+  });
+
+  abpDomInjectionData.forEach(async(dataSet) =>
+  {
+    it("should return correct values for: " + dataSet.testName, async function()
+    {
+      await switchToABPOptionsTab();
+      await browser.newWindow(dataSet.url);
+      const abpInfo = JSON.parse(await browser.
+        executeScript("return document." +
+        "getElementById('__adblock-plus-extension-info').textContent;", []));
+      const dataAbpInfo = JSON.parse(await browser.
+        executeScript("return document.get" +
+        'ElementsByTagName("html")[0].getAttribute' +
+        '("data-adblock-plus-extension-info");', []));
+      expect(abpInfo.isPremium == true).to.be.true;
+      expect(abpInfo.premiumId == "valid_user_id").to.be.true;
+      expect(abpInfo.version == appVersion).to.be.true;
+      expect(abpInfo.id == id).to.be.true;
+      expect(dataAbpInfo.isPremium == true).to.be.true;
+      expect(dataAbpInfo.premiumId == "valid_user_id").to.be.true;
+      expect(dataAbpInfo.version == appVersion).to.be.true;
+      expect(dataAbpInfo.id == id).to.be.true;
+      await browser.closeWindow();
+    });
+  });
+});
